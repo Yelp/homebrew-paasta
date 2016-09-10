892b18d033c503a828d6beb4d417239de73be536
Evan Krall <krall@yelp.com>
2016-09-09 18:02:53 -0700

    make brew audit happy

--- paasta.rb
+++ paasta.rb
@@ -1,9 +1,11 @@
 class Paasta < Formula
   include Language::Python::Virtualenv
 
-  desc "A highly-available, distributed system for building, deploying, and running services using containers and Apache Mesos"
+  desc "Yelp's system for building, deploying, and running services"
+  homepage "https://github.com/Yelp/paasta"
   url "https://github.com/Yelp/paasta/archive/v0.46.4.tar.gz"
   sha256 "9bc7f155c77c3e78c68b3e1c942a836370d20a45687af83a838769c4a0b330d2"
+  head "https://github.com/Yelp/paasta.git"
 
   def install
     venv = virtualenv_create(libexec, "python2.7")
@@ -11,5 +13,9 @@ class Paasta < Formula
     # the Virtualenv object's pip install forces --no-deps, which is really annoying for Paasta, as our
     # requirements.txt does not specify all recursive dependencies, so here we run the pip command we actually want.
     system venv.instance_variable_get(:@venv_root)/"bin/pip", "install", "-r#{buildpath}/requirements.txt"
+  end
+
+  test do
+    system bin/"paasta", "--help"
   end
 end
